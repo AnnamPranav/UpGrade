d1ec375095cc03202af658b0eaa298c148628a2f
Vennela <vennela28082006@gmail.com>
2026-04-24 00:14:39 +0530

    Update interviewRoutes.js

--- backend/routes/interviewRoutes.js
+++ backend/routes/interviewRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Interview = require("../models/interview");
+const Interview = require("../model/interview");
 
 // START INTERVIEW
 router.get("/start", async (req, res) => {
